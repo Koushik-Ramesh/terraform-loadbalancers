6d6a4e5f693a1987ffce07607f9064cd3b00c2e7
Koushik-Ramesh <koushik.r9380@gmail.com>
2024-03-30 16:09:47 +0530

    Autopush

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 module "alb-public" {
   source                    = "./vendor/modules/alb"
   ENV                       = var.ENV
+  ALB_NAME                  = roboshop-public
+  INTERNAL                  = false
 }
 
 module "alb-private" {
   source                    = "./vendor/modules/alb"
   ENV                       = var.ENV
+  ALB_NAME                  = roboshop-private
+  INTERNAL                  = true
 }
 
 
